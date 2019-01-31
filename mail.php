9eae965a75d31b6ef2df6391b46d895df2feff74
OksanaFisenko <mila567_78@mail.ru>
2019-01-31 18:50:48 +0500

    Яндекс-метрика

---
+++ mail.php
@@ -0,0 +1,9 @@
+<?php
+    $name = $_POST['fio'];
+    $phone = $_POST['phone'];
+    $email = $_POST['mail'];
+    
+    echo "Ваше сообщение успешно отправлено, " . $name . "! Твой номер телефона:" . $phone;
+    
+    ?>
+
